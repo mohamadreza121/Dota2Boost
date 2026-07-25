7408768a60e88608d824bc2235e11043cfc3026a
mohamadreza121 <mrheidarpoor7@gmail.com>
2026-07-25 04:10:37 -0400

    load premium Replay Analysis card prototype

--- src/app/services/coaching/layout.tsx
+++ src/app/services/coaching/layout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react";
 import "./radiant-academy-masonry.css";
 import "./radiant-academy-masonry-fix.css";
+import "./radiant-replay-master.css";
 
 export default function CoachingLayout({ children }: { children: ReactNode }) {
   return children;
