827e83a658548ddefe2ee0ec61d9c1a5b9353d53
mohamadreza121 <mrheidarpoor7@gmail.com>
2026-07-25 04:31:46 -0400

    finalize premium stone card stylesheet order

--- src/app/services/coaching/layout.tsx
+++ src/app/services/coaching/layout.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from "react";
 import "./radiant-academy-masonry.css";
 import "./radiant-academy-masonry-fix.css";
 import "./radiant-premium-stone-cards.css";
+import "./radiant-premium-stone-cards-fix.css";
 
 export default function CoachingLayout({ children }: { children: ReactNode }) {
   return children;
