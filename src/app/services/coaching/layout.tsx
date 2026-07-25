339acf6a27c8424ea1f828d5bfb426a355f813ce
mohamadreza121 <mrheidarpoor7@gmail.com>
2026-07-25 03:23:13 -0400

    load masonry stacking correction

--- src/app/services/coaching/layout.tsx
+++ src/app/services/coaching/layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import "./radiant-academy-masonry.css";
+import "./radiant-academy-masonry-fix.css";
 
 export default function CoachingLayout({ children }: { children: ReactNode }) {
   return children;
