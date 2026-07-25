1d956dc15992488fa3ad36a31d703195e7d72dc5
mohamadreza121 <mrheidarpoor7@gmail.com>
2026-07-25 03:21:28 -0400

    load masonry refinement for coaching route

---
+++ src/app/services/coaching/layout.tsx
@@ -0,0 +1,6 @@
+import type { ReactNode } from "react";
+import "./radiant-academy-masonry.css";
+
+export default function CoachingLayout({ children }: { children: ReactNode }) {
+  return children;
+}
