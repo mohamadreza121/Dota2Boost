c3b0ead36475bb369b70bab4e168651775d9fb50
mohamadreza121 <mrheidarpoor7@gmail.com>
2026-07-25 06:51:52 -0400

    load Replay Analysis master card artwork

--- src/app/services/coaching/layout.tsx
+++ src/app/services/coaching/layout.tsx
@@ -3,6 +3,7 @@ import "./radiant-academy-masonry.css";
 import "./radiant-academy-masonry-fix.css";
 import "./radiant-premium-stone-cards.css";
 import "./radiant-premium-stone-cards-fix.css";
+import "./radiant-master-art-card.css";
 
 export default function CoachingLayout({ children }: { children: ReactNode }) {
   return children;
